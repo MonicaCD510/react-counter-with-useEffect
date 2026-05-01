30a554f541489fe5e9684ee8490402a72481257c
Monica Davila <monica.davila510@gmail.com>
2026-05-01 14:27:56 -0500

    add step input for counter

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,25 @@ import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
 
   return (
     <div>
       <h1>Counter</h1>
       <p>Current Count: {count}</p>
 
-      <button onClick={() => setCount(count - 1)}>
-        Decrement
-      </button>
+      <label>Step Value: </label>
+      <input
+        type="number"
+        value={step}
+        onChange={(e) => setStep(Number(e.target.value))}
+      />
 
-      <button onClick={() => setCount(count + 1)}>
-        Increment
-      </button>
+      <br />
+      <br />
+
+      <button onClick={() => setCount(count - step)}>Decrement</button>
+      <button onClick={() => setCount(count + step)}>Increment</button>
     </div>
   );
 }
